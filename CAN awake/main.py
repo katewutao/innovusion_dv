46217d9e4e660eb6fd391517e5e3709e4d25b988
kate <710368479@qq.com>
2023-01-18 17:53:11 +0800

    0118

--- CAN awake/main.py
+++ CAN awake/main.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import sys
 import threading
+import re
 
 
 def downlog(ip,time_path):
@@ -34,9 +35,28 @@ def get_promission(ip):
     child.close()
 
 
+def get_circle_time(dict_config):
+    times=[]
+    for key in dict_config.keys():
+        temp_times=re.findall("(\d+\.?\d*):(\d+\.?\d*)",key)
+        for i in range(len(temp_times)):
+            temp_times[i]=list(temp_times[i])
+            for j in range(len(temp_times[i])):
+                temp_times[i][j]=float(temp_times[i][j])*60
+        times+=temp_times*dict_config[key]
+    return times
+
+
 def set_can(ip):
     command=f'echo "dsp_boot_from can" | nc -nv {ip} 8001'
     os.system(command)
+    
+def cancle_can(ip_list):
+    can=subprocess.Popen(f'exec python3 usbcanfd_controler.py',shell=True)
+    for ip in ip_list:
+        command=f'echo "dsp_boot_from power" | nc -nv {ip} 8001'
+        os.system(command)
+    os.system("ps -ef|grep usbcanfd_controler.py|grep -v grep|awk -F ' ' '{print $2}'|xargs kill -9")
     
     
 
@@ -53,13 +73,14 @@ def load_config():
 
 
 def one_cycle(power_on_time,power_off_time,ip_list,i):
-    print(f"[{str(datetime.datetime.now())}]: current circle {i+1}")
+    print(f"[{str(datetime.datetime.now())}]: current circle {i}")
     time_path=get_time()
     for ip_num in range(len(ip_list)):
         raw_save_path="result/raw/"+ip_list[ip_num].replace('.','_')+'/'+time_path
         subprocess.Popen(f'python3 capture_raw.py -i {ip_list[ip_num]} -s "{raw_save_path}" -l {9100+ip_num} -ls {8100+ip_num}',shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     can=subprocess.Popen(f'exec python3 usbcanfd_controler.py',shell=True)
-    time.sleep(power_on_time-2)
+    if power_on_time>=2:
+        time.sleep(power_on_time-2)
     threads=[]
     for ip in ip_list:
         thread=threading.Thread(target=downlog,args=(ip,time_path,))
@@ -86,19 +107,17 @@ def main(config,log_path):
         cmd=subprocess.Popen(f"exec python3 oneclient.py --ip {ip} --interval 1",shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
         records.append(cmd)
     os.system("python3 lib/set_usbcanfd_env.py demo")
-    
-    if config["mode"]=="time":
-        i=0
-        start_time=time.time()
-        while time.time()-start_time<config["mode_time"]:
-            one_cycle(config["power on time"],config["power off time"],config["lidar_ip"],i)
-            i+=1
-    else:
-        for i in range(config["mode_time"]):
-            one_cycle(config["power on time"],config["power off time"],config["lidar_ip"],i)
+    command='exec python3 power_client.py'
+    cmd_pow=subprocess.Popen(command,stderr=subprocess.PIPE,shell=True)
+    times=get_circle_time(config["time_dict"])
+    i=1
+    for time_one in times:
+        one_cycle(time_one[0],time_one[1],config["lidar_ip"],i)
+        i+=1        
     for record in records:
         record.kill()
-
+    cmd_pow.kill()
+    cancle_can(config["lidar_ip"])
 
 if __name__=="__main__":
     config=load_config()
