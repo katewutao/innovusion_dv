afeb0ae4c43d1de470f24c3ee12dfd3fb0b5ea8a
kate <710368479@qq.com>
2023-01-28 16:55:38 +0800

    0128

--- Power test/IPX7.py
+++ Power test/IPX7.py
@@ -1,13 +1,27 @@
-time_dict={
-    "0:60":0,    #通电时间:断电时间   ,分隔  :循环次数 单位:分钟 :循环次数
-    "1:1":15,
+config={
+    "lidar_ip":[
+        "172.168.1.10",
+        "172.168.1.2",
+        "172.168.1.3",
+        "172.168.1.4",
+        "172.168.1.5",
+        "172.168.1.6",
+        ],  
+    #雷达的ip,格式为英文输入法的双引号,内为ip,以,隔开
+    
+    "time_dict":{
+        "0:60":0,
+        "1:1":15,
+    },
+    #"通电时间:断电时间,通电时间:断电时间,通电时间:断电时间":循环次数
+    #时间单位为分钟
+    
+    "data_num_power_off":10,    #断电时空数据数量
+    "interval_time":5,          #记录雷达状态的时间间隔
+    "timeout_time":5,           #获取雷达连接权限的超时时间
 }
 
 
-interval_time=5             # 上电时记录时间间隔(s)
-data_num_power_off=10       # 断电时或通电0s空值数量
-timeout_time=5
-
 from dv import dv_test
 if __name__=="__main__":
-    dv_test(time_dict,interval_time,data_num_power_off,timeout_time)
+    dv_test(config)
