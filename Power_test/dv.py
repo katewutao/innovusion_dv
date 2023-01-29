a68c0fcdaf5725ee26fecb7105e715507c6b868e
kate <710368479@qq.com>
2023-01-29 09:45:42 +0800

    0129
    Former-commit-id: 2707f828410c6c42131c60efa6f9e8e8eaea4bfe

--- Power_test/dv.py
+++ Power_test/dv.py
@@ -44,9 +44,9 @@ def init_power():
     res=cmd.read()
     if os.path.exists("power.py"):
         os.remove("power.py")
-    if "Future Technology Devices International, Ltd FT232 Serial (UART) IC" in res:
+    if "FT232" in res:
         shutil.copyfile(os.path.join(os.getcwd(),"power_DH.py"),os.path.join(os.getcwd(),"power.py"))
-    elif "QinHeng Electronics HL-340 USB-Serial adapter" in res:
+    elif "HL-340" in res:
         shutil.copyfile(os.path.join(os.getcwd(),"power_PY.py"),os.path.join(os.getcwd(),"power.py"))
     else:
         print("power is not PY or DH")
