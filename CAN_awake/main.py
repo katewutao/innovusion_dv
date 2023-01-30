ed1b5dc97ee263bbcb83759b5d3cd6ff804b50a4
kate <710368479@qq.com>
2023-01-30 10:36:46 +0800

    0130
    Former-commit-id: d7a5dab1b52a737a2b5f224cc5ffd71e61462434

--- CAN_awake/main.py
+++ CAN_awake/main.py
@@ -204,6 +204,16 @@ def main(config,log_path):
         i+=1 
     cmd_pow.kill()
     cancle_can(config["lidar_ip"])
+    import power
+    while True:
+        try:
+            pow=power.Power()
+            pow.power_off()
+            break
+        except:
+            print(f"[{datetime.datetime.now()}]get power permission")
+            os.system('sshpass -p demo sudo python3 ./power.py')
+            pass
 
 if __name__=="__main__":
     config={
