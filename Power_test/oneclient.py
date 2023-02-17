8b8460546b3d1b9307d98e4aab2841bc2491e187
kate <710368479@qq.com>
2023-02-17 15:52:19 +0800

    oneclient pluto
    Former-commit-id: 6eb05386b6c034eb54907958d4b875979d7416fe

--- Power_test/oneclient.py
+++ Power_test/oneclient.py
@@ -19,9 +19,9 @@ import datetime
 # polygon speed,Motor DC bus voltage,Motor RMS current,Motor speed control err,Galvo FPS,Galvo RMS current,Galvo frame counter,Galvo position control err,laser current,unit current,
 
 save_path = os.getcwd()+'/result/'
-record_keys = ['time', 'T0', 'T1', 'T2', 'Tlaser', 'Txadc', 'A=', 'B=', 'C=', 'D=', 'SP: ', 'polygon speed:', 'Motor DC bus voltage:', 'Motor RMS current:', 'Motor speed control err:',
-               'Galvo FPS:', 'Galvo RMS current:', 'Galvo frame counter:', 'Galvo position control err:', 'laser current:', 'unit current:', 'LASER current', 'temperature', 'pump_st', 'alarm','get-ref-intensity','get-fpga-intensity','vol', 'curr']
-record_header = "time,temp_board,temp_adc1,temp_adc2,Temp_laser,Temp_fpga,temp_A,temp_B,temp_C,temp_D,motor speed,polygon speed,Motor DC bus voltage,Motor RMS current,Motor speed control err,Galvo FPS,Galvo RMS current,Galvo frame counter,Galvo position control err,laser current,unit current,LASER current,laser temp,pump_st,alarm,CHA_ref,CHB_ref,CHC_ref,CHD_ref,CHA_fpga,CHB_fpga,CHC_fpga,CHD_fpga,vol,curr"
+record_keys = ['time', 'T0', 'T1', 'T2', 'Tlaser', 'Txadc', 'A=', 'B=', 'C=', 'D=', 'SP: ', 'LASER current','pump voltage=','temperature=','seed temperature=','polygon speed:', 'Motor DC bus voltage:', 'Motor RMS current:', 'Motor speed control err:',
+               'Galvo FPS:', 'Galvo RMS current:', 'Galvo frame counter:', 'Galvo position control err:', 'laser current:', 'unit current:', 'temperature', 'pump_st', 'alarm','get-ref-intensity','get-fpga-intensity','vol', 'curr']
+record_header = "time,temp_board,temp_adc1,temp_adc2,Temp_laser,Temp_fpga,temp_A,temp_B,temp_C,temp_D,motor speed,Pump_laser_current,pump_voltage,laser_module_temperature,seed_temperature,polygon_speed,Motor_DC_bus_voltage,Motor_RMS_current,Motor_speed_control_err,Galvo_FPS,Galvo_RMS_current,Galvo_frame_counter,Galvo_position_control_err,laser_module_current,unit_current,laser_temp,pump_st,alarm,CHA_ref,CHB_ref,CHC_ref,CHD_ref,CHA_fpga,CHB_fpga,CHC_fpga,CHD_fpga,vol,curr"
 
 def extract(keys, st):
     import re
@@ -46,8 +46,7 @@ def extract(keys, st):
     return res
 
 
-def get_command_result(command):
-    command = 'curl http://'+arg.ip+':8088/get-all-status'
+def get_command_result(command,save_log):
     cmd = subprocess.Popen(command, shell=True,
                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     time.sleep(0.5)
@@ -58,15 +57,13 @@ def get_command_result(command):
         res = ''
         res1 = 'failed'
     cmd.kill()
-    write_log(save_path+'testlog' +
-              arg.ip.replace('.', '_')+'.txt', command, res1)
+    write_log(save_log, command, res1)
     return res
 
 
 def write_log(txt_path, command, res):
     file_txt = open(txt_path, 'a', encoding='utf-8', newline='\n')
-    file_txt.write(time.strftime('%Y.%m.%d %H:%M:%S ',
-                   time.localtime(time.time()))+'  '+command+'  '+res+'\n')
+    file_txt.write(f'{datetime.datetime.now()}   {command}   {res}\n')
     file_txt.close()
 
 
@@ -78,42 +75,13 @@ def csv_write(file, lis):
     file_read = open(file, 'a', newline='\n')
     str1 = ''
     for i in range(len(lis)):
-        if i != len(lis)-1:
-            str1 = str1+str(lis[i])+','
-        else:
-            str1 = str1+str(lis[i])
+        str1+=f'{lis[i]},'
+    str1=str1[:-1]
     if flag:
         file_read.write('\n'+str1)
     else:
         file_read.write(str1)
     file_read.close()
-
-def main(arg):
-    command = f"curl http://{arg.ip}:8088/get-all-status"
-    csv_path=os.path.join(save_path,f"record_{arg.ip.replace('.', '_')}.csv")
-    power_path=os.path.join(save_path,'pow_status.csv')
-    if not os.path.exists(csv_path):
-        with open(csv_path, 'w', newline='\n') as f:
-            f.write(record_header)
-    while True:
-        t=time.time()
-        times=str(datetime.datetime.now())
-        res = get_command_result(command)
-        temp = []
-        temp.append(times)
-        temp+=extract(record_keys[1:-2], res)
-        while 1:
-            try:
-                pow = pd.read_csv(power_path, header=None).values.tolist()
-                break
-            except:
-                continue
-        temp+=pow[0]
-        csv_write(csv_path, temp)
-        sleep_time=arg.interval-time.time()+t
-        if sleep_time>0:
-            time.sleep(sleep_time)
-
 
 if __name__=="__main__":
     parse = argparse.ArgumentParser()
@@ -121,4 +89,29 @@ if __name__=="__main__":
     parse.add_argument('--interval', type=float, required=True,
                     help='record time interval')
     arg = parse.parse_args()
-    main(arg)
+    ip_name=arg.ip.replace('.', '_')
+    save_log=os.path.join(save_path,f"testlog_{ip_name}.txt")
+    save_csv=os.path.join(save_path,f"record_{ip_name}.csv")
+    power_csv=os.path.join(save_path,'pow_status.csv')
+    if not os.path.exists(save_csv):
+        file = open(save_csv, 'w', newline='\n')
+        file.write(record_header)
+        file.close()
+    command = f'curl http://{arg.ip}:8088/get-all-status'
+    while True:
+        t=time.time()
+        res = get_command_result(command,save_log)
+        temp = [datetime.datetime.now()]
+        temp+=extract(record_keys[1:-2], res)
+        while 1:
+            try:
+                pow = pd.read_csv(power_csv, header=None).values.tolist()
+                break
+            except:
+                continue
+        temp.append(pow[0][0])
+        temp.append(pow[0][1])
+        csv_write(save_csv, temp)
+        sleep_time=arg.interval-time.time()+t
+        if sleep_time>0:
+            time.sleep(sleep_time)
