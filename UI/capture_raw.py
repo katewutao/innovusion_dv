98711b04bc7bbdc79cfe20aca9c3bafcfd77b443
kate <710368479@qq.com>
2023-05-05 15:52:17 +0800

    add UI tool

---
+++ UI/capture_raw.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import subprocess
+import time,datetime,select
+import sys
+import re
+
+
+
+def ping(ip,time_interval):
+    if 'linux' in sys.platform:
+        cmd=subprocess.Popen(f'exec ping -c 1 {ip}',shell=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        time.sleep(time_interval)
+        if cmd.poll() is not None:
+            res = cmd.stdout.read().decode('utf-8')
+        else:
+            res = ''
+        cmd.kill()
+        if "100%" in res or res=='':
+            return False
+        else:
+            return True
+    else:
+        cmd=subprocess.Popen(f'ping -n 1 -w 100 {ip}',shell=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmd.wait()
+        res = cmd.stdout.read()
+        cmd.kill()
+        if b"100%" in res:
+            return False
+        else:
+            return True
+
+
+def newest_folder(A,B):
+    newest_path=os.path.join(A,str(B))
+    return newest_path
+
+def check_raw(file_list):
+    key="sn\d+-\d+.*\.inno_raw$"
+    for file in file_list:
+        if re.match(key,file):
+            return True
+    return False
+
+def delete_util_log(log_path):
+    log_path=os.path.abspath(log_path)
+    if os.path.exists(log_path):
+        try:
+            os.remove(log_path)
+        except:
+            pass
+
+
+def get_cmd_print(cmd,poll_obj,fault_log_path):
+    if poll_obj.poll(0):
+        stderr=cmd.stderr.readline()
+        ret=re.search("(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{0,3}).+?fault_manager.cpp.+?\s([A-Z_0-9]+).+?has\sbeen\sset",stderr)
+        if ret:
+            str1=f"[{datetime.datetime.now()}] {args.ip} {ret.group(2)} has been set"
+            with open(fault_log_path,"a") as f:
+                f.write(str1+"\n")
+            print("\033[0;31;40m",str1, "\033[0m")
+    stdout=cmd.stdout.readline()
+    ret=re.search("(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{0,3}).+?fault_manager.cpp.+?\s([A-Z_0-9]+).+?has\sbeen\sheal",stdout)
+    if ret:
+        str1=f"[{datetime.datetime.now()}] {args.ip} {ret.group(2)} has been heal"
+        with open(fault_log_path,"a") as f:
+            f.write(str1+"\n")
+        print("\033[1;32m",str1, "\033[0m")
+
+
+
+def main(args):
+    util_dir="lidar_util"
+    util_path=os.path.join(util_dir,"inno_pc_client")
+    fault_log_path=os.path.join(args.faultpath,"fault")
+    if not os.path.exists(fault_log_path):
+        os.makedirs(fault_log_path)
+    fault_log_path=os.path.join(fault_log_path,args.ip.replace(".","_")+".txt")
+    if not os.path.exists(util_path):
+        print(f"file {util_path} not exists!")
+        return None
+    while True:
+        if ping(args.ip,0.3):
+            break
+    if not os.path.exists(args.savepath):
+        os.makedirs(args.savepath)
+    i=1
+    newest_path=newest_folder(args.savepath,i)
+    command1=f"{util_path} --lidar-ip {args.ip} --lidar-port 8010 --lidar-udp-port {args.lidarudpport} --tcp-port {args.lidarport}"
+    command2=f"curl localhost:{args.lidarport}/command/?set_raw_data_save_path='{newest_path}'"
+    command3=f"curl localhost:{args.lidarport}/command/?set_faults_save_raw=ffffffffffffffff"
+    command4=f"curl localhost:{args.lidarport}/command/?set_save_raw_data={args.lisenport}"
+    # cmd=subprocess.Popen(command1,shell=True,stdout=open(os.path.join(util_dir,f"{args.ip}_out"),'w'),stderr=open(os.path.join(util_dir,f"{args.ip}_err"),'w'))
+    cmd=subprocess.Popen(command1,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,universal_newlines=True)
+    poll_obj=select.poll()
+    poll_obj.register(cmd.stderr,select.POLLIN)
+    time.sleep(1)
+    os.system(command2)
+    os.system(command3)
+    os.system(command4)
+    raw_count=len(os.listdir(args.savepath))
+    while True:
+        get_cmd_print(cmd,poll_obj,fault_log_path)
+        delete_util_log(os.path.join(util_dir,f"{args.ip}_out"))
+        delete_util_log(os.path.join(util_dir,f"{args.ip}_err"))
+        delete_util_log(os.path.join(util_dir,"inno_pc_client.log"))
+        delete_util_log(os.path.join(util_dir,"inno_pc_client.log.err"))
+        delete_util_log(os.path.join(util_dir,"inno_pc_client.log.1"))
+        delete_util_log(os.path.join(util_dir,"inno_pc_client.log.2"))
+        if not os.path.exists(newest_path):
+            print(f"inno_pc_client boot failed!")
+            return None
+        if check_raw(os.listdir(newest_path)):
+            if i>=raw_count:
+                print(f"record raw data to {os.path.abspath(newest_path)}")
+            i+=1
+            newest_path=newest_folder(args.savepath,i)
+            command2=f"curl localhost:{args.lidarport}/command/?set_raw_data_save_path='{newest_path}'"
+            os.system(command2)
+            os.system(command3)
+
+if __name__=="__main__":
+    parses=argparse.ArgumentParser()
+    parses.add_argument('--ip','-i',type=str,required=True)
+    parses.add_argument('--savepath','-s',type=str,required=True)
+    parses.add_argument('--faultpath','-f',type=str,required=True)
+    parses.add_argument('--lidarport','-l',type=str,required=True)
+    parses.add_argument('--lidarudpport','-lup',type=str,required=True)
+    parses.add_argument('--lisenport','-ls',type=str,required=True)
+    args=parses.parse_args()
+    main(args)
---
+++ UI/project/Nio.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python3
+
+# /**
+#  * @author katewutao
+#  * @email kate.wu@cn.innovusion.com
+#  * @create date 2022-03-22 16:48:51
+#  * @modify date 2022-11-15 13:36:53
+#  * @desc [description]
+#  */
+
+
+import argparse
+import time
+import os
+import pandas as pd
+import subprocess
+import datetime,platform
+
+# polygon speed,Motor DC bus voltage,Motor RMS current,Motor speed control err,Galvo FPS,Galvo RMS current,Galvo frame counter,Galvo position control err,laser current,unit current,
+
+record_keys = ['time','SN','CustomerSN','T0', 'T1', 'T2', 'Tlaser', 'A=', 'B=', 'C=', 'D=', 'SP: ', 'LASER current','pump voltage=','temperature=','seed temperature=','polygon speed:', 'Motor DC bus voltage:', 'Motor RMS current:', 'Motor speed control err:',
+               'Galvo FPS:', 'Galvo RMS current:', 'Galvo frame counter:', 'Galvo position control err:','Board humidity','laser current:', 'unit current:', 'temperature', 'pump_st', 'alarm','get-ref-intensity','get-fpga-intensity','vol', 'curr']
+record_header = "time,SN,CustomerSN,Temp_fpga,temp_adc,temp_board,Temp_laser,temp_A,temp_B,temp_C,temp_D,motor speed,Pump_laser_current,pump_voltage,laser_module_temperature,seed_temperature,polygon_speed,Motor_DC_bus_voltage,Motor_RMS_current,Motor_speed_control_err,Galvo_FPS,Galvo_RMS_current,Galvo_frame_counter,Galvo_position_control_err,Board humidity,laser_module_current,unit_current,laser_temp,pump_st,alarm,CHA_ref,CHB_ref,CHC_ref,CHD_ref,CHA_fpga,CHB_fpga,CHC_fpga,CHD_fpga,vol,curr"
+
+
+def extract(keys, st):
+    import re
+    res=[]
+    for i in range(len(keys)):
+        if "intensity" not in keys[i]:
+            ret=re.search(keys[i]+".*?(-?\d+\.?\d*)",st)
+            if ret:
+                res.append(ret.group(1))
+            else:
+                res.append(-100)
+        else:
+            ret=re.search(keys[i]+".*?(\d+).*?(\d+).*?(\d+).*?(\d+)",st)
+            if ret:
+                for j in range(4):
+                    res.append(ret.group(j+1))
+            else:
+                for j in range(4):
+                    res.append(-100)
+    return res
+
+
+def get_command_result(command,save_log):
+    cmd = subprocess.Popen(command, shell=True,
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    time.sleep(0.5)
+    if cmd.poll() is not None:
+        res = str(cmd.stdout.read())
+        res1 = 'success'
+    else:
+        res = ''
+        res1 = 'failed'
+    cmd.kill()
+    write_log(save_log, command, res1)
+    return res
+
+
+def ping(ip,time_interval):
+    if 'windows' not in platform.platform().lower():
+        cmd=subprocess.Popen(f'exec ping -c 1 {ip}',shell=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        time.sleep(time_interval)
+        if cmd.poll() is not None:
+            res = cmd.stdout.read().decode('utf-8')
+        else:
+            res = ''
+        cmd.kill()
+        if "100%" in res or res=='':
+            return False
+        else:
+            return True
+    else:
+        cmd=subprocess.Popen(f'ping -n 1 -w 100 {ip}',shell=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmd.wait()
+        res = cmd.stdout.read()
+        cmd.kill()
+        if b"100%" in res:
+            return False
+        else:
+            return True
+
+
+
+def write_log(txt_path, command, res):
+    file_txt = open(txt_path, 'a', encoding='utf-8', newline='\n')
+    file_txt.write(f'{datetime.datetime.now()}   {command}   {res}\n')
+    file_txt.close()
+
+
+def csv_write(file, lis):
+    if not os.path.exists(file):
+        str1 = ""
+    else:
+        str1 = '\n'
+    for i in range(len(lis)):
+        str1+=f'{lis[i]},'
+    str1=str1[:-1]
+    with open(file, 'a', newline='\n') as f:
+        f.write(str1)
+
+def get_customerid(ip):
+    customerid = ''
+    import socket
+    command = 'mfg_rd "CustomerSN"\n'
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, 8001))
+    s.settimeout(1)
+    s.sendall(command.encode())
+    try:
+        data=s.recv(1024)
+        lst = data.decode('ascii').split('\n')
+        for string in lst:
+            if ':' in string:
+                # print(string)
+                customerid = string.split(':')[1].split('"')[1]
+    except Exception as e:
+        print(e)
+    s.close()
+    return customerid 
+
+
+def get_sn(ip):
+    command=f"curl {ip}:8010/command/?get_sn"
+    cmd = subprocess.Popen(command, shell=True,
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE,universal_newlines=True)
+    
+    res=cmd.communicate()
+    SN=res[0]
+    
+    #SN=cmd.stdout.read()
+    
+    return SN 
+
+
+
+def one_record(ip,save_log,SN,CustomerSN,power_csv):
+    command = f'curl http://{ip}:8088/get-all-status'
+    res = get_command_result(command,save_log)
+    temp = [f" {datetime.datetime.now()}",SN,CustomerSN]
+    temp+=extract(record_keys[len(temp):-2], res)
+    if res=="":
+        return None
+    while 1:
+        try:
+            pow = pd.read_csv(power_csv, header=None).values.tolist()
+            break
+        except:
+            continue
+    temp.append(pow[0][0])
+    temp.append(pow[0][1])
+    return temp
