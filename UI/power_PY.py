f5f8e34eb56cc61d91cab6473e919f6c807e9cf2
kate <710368479@qq.com>
2023-05-26 17:55:26 +0800

    add select power type
    Former-commit-id: 5ffb3b2b301df5d8d0d31a1c895cb259f4470de1

--- UI/power_PY.py
+++ UI/power_PY.py
@@ -36,43 +36,26 @@ class Power(object):
         result = master.execute(1, cst.READ_INPUT_REGISTERS, 1007, 1)  # get status register
         power = int(result[0]) & 1
         if power == 0:
-            #print("The PS output is off normally.")
             time.sleep(8)
             result = master.execute(1, cst.READ_INPUT_REGISTERS, 1000, 2)  # get voltage & current
-            vol = float(int(result[0]) / 100)
-            cur = float(int(result[1]) / 100)
-            #print("The read output voltage is: %.2f and current is：%.2f." % (vol, cur))
         else:
             print("Error! The PS output off failed.")
-
-        #print("Now start waiting after power off......")
         return 0
 
     def power_on(self):
-        #print("Now start waiting before power on......")
         master = modbus_rtu.RtuMaster(serial.Serial(self.com, 9600, 8, 'N', 1))
         master.set_timeout(2.0)
         master.set_verbose(True)
 
-        # set output voltage=13.50V, current=10.00A
         result = master.execute(1, cst.READ_HOLDING_REGISTERS, 2001, 2)  # get the vol & current of setting
-        vol = float(int(result[0]) / 100)
-        cur = float(int(result[1]) / 100)
-        #print("The setting voltage is: %.2f and current is：%.2f." % (vol, cur))
 
         master.execute(1, cst.WRITE_MULTIPLE_REGISTERS, 2016, output_value=[65535])  # output on
         result = master.execute(1, cst.READ_INPUT_REGISTERS, 1007, 1)  # get status register
         power = int(result[0]) & 1  # power[0]: 0-output off, 1-output on
         if power == 1:
-            #print("The PS output is power on normally.")
             result = master.execute(1, cst.READ_INPUT_REGISTERS, 1000, 2)  # get output voltage & current
-            vol = float(int(result[0]) / 100)
-            cur = float(int(result[1]) / 100)
-            #print("The PS output voltage is: %.2f and current is：%.2f." % (vol, cur))
         else:
             print("Error! The PS output on failed!")
-
-        #print("Now start waiting while PS powers on......")
         return 0
     
     def PowerStatus(self):
@@ -82,19 +65,18 @@ class Power(object):
         result = master.execute(1, cst.READ_INPUT_REGISTERS, 1007, 1)  # get status register
         power = int(result[0]) & 1  # power[0]: 0-output off, 1-output on
         if power == 1:
-            #print("The PS output is power on normally.")
             result = master.execute(1, cst.READ_INPUT_REGISTERS, 1000, 2)  # get output voltage & current
-            # vol = float(int(result[0]) / 100)
-            # cur = float(int(result[1]) / 100)
-            # print("The PS output voltage is: %.2f and current is：%.2f." % (vol, cur))
         else:
             print("Error! The PS output on failed!")
-        return result
-    
+        if (isinstance(result,tuple) or isinstance(result,list)) and len(result)>=2:
+            return [result[0]/100,result[1]/100]
+        return None
+        
     def set_voltage(self,voltage):
         master = modbus_rtu.RtuMaster(serial.Serial(self.com, 9600, 8, 'N', 1))
+        voltage=int(voltage*100)
         try:
-            master.execute(1, cst.WRITE_MULTIPLE_REGISTERS, 2021, output_value=[voltage*100,(900//voltage)*100])
+            master.execute(1, cst.WRITE_MULTIPLE_REGISTERS, 2021, output_value=[voltage,int(9000000//voltage)])
         except:
             pass
     
