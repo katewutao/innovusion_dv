54fd9197b9f8ed890889c9202873d12842849521
kate <710368479@qq.com>
2023-07-27 10:16:34 +0800

    add multi_command in util
    Former-commit-id: 024a6bfb21d319ac2bf10778ff235c105b1c0adc

--- UI/test.py
+++ UI/test.py
@@ -1,16 +1,23 @@
 import numpy as np
 import pandas as pd
-import subprocess
+import subprocess,time
 
-def main():
-    command="curl --connect-timeout 2 172.168.1.10:8088/get-all-status"
-    cmd=subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    try:
-        cmd.wait(2)
-        res=str(cmd.stdout.read())
-    except:
-        res=""
-    cmd.kill()
-    return res
+# def main():
+#     command="curl --connect-timeout 2 172.168.1.10:8088/get-all-status"
+#     cmd=subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+#     try:
+#         cmd.wait(2)
+#         res=str(cmd.stdout.read())
+#     except:
+#         res=""
+#     cmd.kill()
+#     return res
+# while True:
+#     print(main())
+
+
+command="curl --connect-timeout 2 172.168.1.10:8088/get-all-status"
+cmd=subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 while True:
-    print(main())
+    print(not cmd.poll())
+    time.sleep(1)
