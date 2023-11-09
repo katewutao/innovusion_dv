c4eab4c35c68d162a0adb446d51871c1ccc0d4ba
kate <710368479@qq.com>
2023-11-09 16:14:58 +0800

    add cmd kill method
    Former-commit-id: e1ec4c9ef068763b60eabbd959a9fddac2d76fc4

--- update.py
+++ update.py
@@ -1,11 +1,13 @@
 import subprocess
 from UI.utils import kill_subprocess
+import time
 
 command = "git pull -f"
 while True:
     cmd = subprocess.Popen(command, shell=True)
-    try:
-        cmd.wait(3)
-        break
-    except:
+    time.sleep(5)
+    if cmd.poll() is None:
         kill_subprocess(command)
+        cmd.communicate()
+    else:
+        break
