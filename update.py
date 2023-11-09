42c80e50f0c4dc2d7893644f6a6846602d231357
kate <710368479@qq.com>
2023-11-09 16:17:51 +0800

    def function of update
    Former-commit-id: 816ae159fc26fd8b84d116ff497a959a2bd37090

--- update.py
+++ update.py
@@ -2,12 +2,16 @@ import subprocess
 from UI.utils import kill_subprocess
 import time
 
-command = "git pull -f"
-while True:
-    cmd = subprocess.Popen(command, shell=True)
-    time.sleep(5)
-    if cmd.poll() is None:
-        kill_subprocess(command)
-        cmd.communicate()
-    else:
-        break
+def update(command,timeout = 5):
+    while True:
+        cmd = subprocess.Popen(command, shell=True)
+        time.sleep(timeout)
+        if cmd.poll() is None:
+            kill_subprocess(command)
+            cmd.communicate()
+        else:
+            break
+
+if __name__ == "__main__":
+    command = "git pull -f"
+    update(command)
